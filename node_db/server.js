7cebc97132b103e0799d8cf77bb0986776b3c912
Windows10 <denrokokasdi@gmail.com>
2023-07-14 11:29:52 +0800

    update env db

--- node_db/server.js
+++ node_db/server.js
@@ -2,7 +2,6 @@ import app from "./app.js";
 const PORT = 8080;
 
 app.listen(PORT, () => {
-	console.log(
-		`Server run on port 8080, Database info, ${process.env.DB_HOST} ${process.env.DB_USER} ${process.env.DB_DATABASE} ${process.env.DB_PASSWORD}`
-	);
+	console.log(`Server run on port 8080`);
 });
+//${process.env.DB_HOST} ${process.env.DB_USER} ${process.env.DB_DATABASE} ${process.env.DB_PASSWORD}
